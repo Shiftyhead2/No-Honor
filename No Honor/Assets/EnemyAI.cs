92432361024c5606148b967b138607e1f0a9d588
Shiftyhead2 <shiftyhead2@gmail.com>
2019-08-03 16:20:44 +0200

    Added enemy AI and shooting

--- No Honor/Assets/EnemyAI.cs
+++ No Honor/Assets/EnemyAI.cs
@@ -6,8 +6,14 @@ public class EnemyAI : MonoBehaviour
 {
     public Transform Player;
     public float moveSpeed;
+    public float StoppingDistance;
+    public float RetreatDistance;
     private Rigidbody2D rb;
     private Vector2 Movement;
+    private float TimeBtwShots;
+    public float StartTimeBtwShots;
+    public GameObject ShadowStar;
+    public Transform ShootPoint;
    
 
     // Start is called before the first frame update
@@ -15,6 +21,7 @@ public class EnemyAI : MonoBehaviour
     {
         Player = GameObject.FindGameObjectWithTag("Player").transform;
         rb = GetComponent<Rigidbody2D>();
+        TimeBtwShots = StartTimeBtwShots;
 
     }
 
@@ -23,6 +30,38 @@ public class EnemyAI : MonoBehaviour
     {
         Vector3 direction = Player.position - transform.position;
         Movement = direction.normalized;  
+        if(Vector2.Distance(transform.position,Player.position) > StoppingDistance)
+        {
+            moveSpeed = 1f;
+        }else if(Vector2.Distance(transform.position,Player.position) < StoppingDistance && Vector2.Distance(transform.position,Player.position) > RetreatDistance)
+        {
+            moveSpeed = 0f;
+            if (TimeBtwShots <= 0f)
+            {
+                Instantiate(ShadowStar, ShootPoint.position, Quaternion.identity);
+                TimeBtwShots = StartTimeBtwShots;
+            }
+            else
+            {
+                TimeBtwShots -= Time.deltaTime;
+            }
+        }
+        else if(Vector2.Distance(transform.position,Player.position)< RetreatDistance)
+        {
+            moveSpeed = -1f;
+            if (TimeBtwShots <= 0f)
+            {
+                Instantiate(ShadowStar, transform.position, Quaternion.identity);
+                TimeBtwShots = StartTimeBtwShots;
+            }
+            else
+            {
+                TimeBtwShots -= Time.deltaTime;
+            }
+        }
+
+        
+
     }
 
 
---
+++ No Honor/Assets/Projective.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projective : MonoBehaviour
+{
+    public float Speed;
+    private Transform Player;
+    private Vector2 target;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        target = new Vector2(Player.position.x, Player.position.y);
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target, Speed * Time.deltaTime);
+
+        if(transform.position.x == target.x && transform.position.y == target.y)
+        {
+            DestroyProjective();
+        }
+        
+    }
+
+    void DestroyProjective()
+    {
+        Destroy(gameObject);
+    }
+}
